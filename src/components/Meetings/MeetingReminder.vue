8ae1bebfe6ebf4ab25984ea165be19867fb7784f
RAVSPW <34798524+RAVSPW@users.noreply.github.com>
2020-05-03 12:07:59 +0200

    update MeetingReminder

--- src/components/Meetings/MeetingReminder.vue
+++ src/components/Meetings/MeetingReminder.vue
@@ -1,1 +1,15 @@
-
+<template>
+    <v-dialog
+            persistent
+            v-model="meetingReminderVisibility"
+            :max-width="meetingReminderWidth"
+    >
+        <div id="information-dialog-card">
+            <h3 v-for="meeting in meetings" :key="meeting.index">Masz dziś spotkanie {{meeting.title}} o {{meeting.date}} w {{meeting.location}}</h3>
+            <div class="row">
+                <v-btn @click="closeMeetingReminder" class="teal--text font-weight-bold" rounded color="#EEEEEE" dark>OK
+                </v-btn>
+            </div>
+        </div>
+    </v-dialog>
+</template>
