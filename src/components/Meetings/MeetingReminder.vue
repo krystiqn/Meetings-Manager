36b3f045b3d8ceb70488c02978b199ed16ab84b4
RAVSPW <34798524+RAVSPW@users.noreply.github.com>
2020-05-03 12:09:25 +0200

    changes MeetingReminder

--- src/components/Meetings/MeetingReminder.vue
+++ src/components/Meetings/MeetingReminder.vue
@@ -13,3 +13,55 @@
         </div>
     </v-dialog>
 </template>
+
+<script>
+    export default {
+        name: 'MeetingReminder',
+        props: {
+            meetingReminderVisibility: Boolean,
+            meetings: Array
+        },
+        methods: {
+            closeMeetingReminder () {
+                this.$emit('closeMeetingReminder', false)
+            }
+        },
+        computed: {
+            meetingReminderWidth () {
+                if (this.$vuetify.breakpoint.xsOnly) {
+                    return 60 + '%'
+                } else {
+                    return 50 + '%'
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #information-dialog-card {
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+        padding: 1rem;
+        background: gray;
+        overflow-y: hidden;
+    }
+    h3 {
+        font-style: normal;
+        font-weight: 500;
+        text-align: center;
+    }
+    .row {
+        margin-right: 0;
+        margin-left: 0;
+        margin-bottom: 0.5rem;
+        flex-direction: row;
+        align-content: center;
+        justify-content: center;
+        width: 100%;
+        padding-top: 0.5rem;
+        text-align: center;
+        padding-bottom: 0.3rem;
+    }
+</style>
